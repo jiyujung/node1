036bb6cc05d7719462d3ec97df8737d594ae9803
jiyujung <s2019w39@e-mirim.hs.kr>
2021-03-17 09:06:43 +0900

    web2-5

--- main.js
+++ main.js
@@ -2,12 +2,24 @@
 // let 확실히 값이 바뀌는 것
 // 바뀔지 안바뀔지 애매할 때 -> 일단 const로 선언해봄
 
+// node.js가 제공한 api
 const http = require('http');
+const fs = require('fs');
 
 const app = http.createServer(function (req, res){
+    let url = req.url;
+    if(url == '/')
+        url = '/index.html';
+    if(url == '/favicon.ico')
+        return res.writeHead(404);
+
+    // header로 보내는 부분
     res.writeHead(200);
-    console.log(__dirname + req.url);
-    res.end("hello egoing");
+    res.end(fs.readFileSync(__dirname + url));
+    //console.log(__dirname + req.url);    // 서버에서 이 파일의 내용을 그대로 보내겠다
+    // res.end("hello egoing");
+    // body로 보내는 부분
+    //res.end("hello egoing");
 })
 
 app.listen(3000);
