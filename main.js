2a902a5e29ebe560f4cba0d29bd471556c22c99f
jiyujung <s2019w39@e-mirim.hs.kr>
2021-03-18 10:10:04 +0900

    web2-19

--- main.js
+++ main.js
@@ -1,59 +1,69 @@
-// const 확실히 값이 안바뀌는것
-// let 확실히 값이 바뀌는것
-// 바뀔지 안바뀔지 애매할 때 -> 일단 const로 선언해봄
-
-// node.js가 제공한 api
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const app = http.createServer(function (req, res) {
-    let _url = req.url
-    const queryData = url.parse(_url, true).query
-    //console.log(queryData.id);
-    let title = queryData.id;
-    let filePath = queryData.id;
-
-    if (_url === '/') {
-        //_url = '/index.html';
-        title = 'Welcome'
-        filePath = 'welcome';
+const app = http.createServer(function (request, response) {
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            //fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+            const title = 'Welcome';
+            const description = 'Hello, Node.js';
+            const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+            response.writeHead(200);
+            response.end(template);
+            //  });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                const title = queryData.id;
+                const template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
     }
-    if (_url === '/favicon.ico'){
-        res.writeHead(404, {"content-Type": "text/palin"});
-        res.write("404 Not found");
-        res.end();
-    }
 
-    // header로 보내는 부분
-    res.writeHead(200)
-    fs.readFile(`data/${filePath}`, 'utf8', function (err, data) {
-        const template = `
-<!doctype html>
-<html lang="ko">
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="index.html">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>${data}</p>
-</body>
-</html>
-  `;
-        res.end(template);
-    });
-    //res.end(queryData.id);
-    //res.end(fs.readFileSync(__dirname + url));
-    //console.log(__dirname + req.url);    // 서버에서 이 파일의 내용을 그대로 보내겠다
-    // res.end("hello egoing");
-    // body로 보내는 부분
-    //res.end("hello egoing");
+
 });
 app.listen(3000);
