4d4f4c37993849383052540aa38ef2b01b8f7256
jiyujung <s2019w39@e-mirim.hs.kr>
2021-03-18 09:59:08 +0900

    web2-19

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-// const 확실하게 값이 안바뀌는 것
-// let 확실히 값이 바뀌는 것
+// const 확실히 값이 안바뀌는것
+// let 확실히 값이 바뀌는것
 // 바뀔지 안바뀔지 애매할 때 -> 일단 const로 선언해봄
 
 // node.js가 제공한 api
 const http = require('http');
-//const fs = require('fs');
+const fs = require('fs');
 const url = require('url');
 
-const app = http.createServer(function (req, res){
-    var _url = req.url;
-    const queryData = url.parse(_url, true).query;
+const app = http.createServer(function (req, res) {
+    let _url = req.url
+    const queryData = url.parse(_url, true).query
     //console.log(queryData.id);
     let title = queryData.id;
+    let filePath = queryData.id;
 
-    if(_url === '/')
+    if (_url === '/') {
         //_url = '/index.html';
-        title = 'Welcome';
-    if(_url === '/favicon.ico')
-        return res.writeHead(404);
+        title = 'Welcome'
+        filePath = 'welcome';
+    }
+    if (_url === '/favicon.ico'){
+        res.writeHead(404, {"content-Type": "text/palin"});
+        res.write("404 Not found");
+        res.end();
+    }
 
     // header로 보내는 부분
-    res.writeHead(200);
-    //res.end(queryData.id);
-    const template = `
+    res.writeHead(200)
+    fs.readFile(`data/${filePath}`, 'utf8', function (err, data) {
+        const template = `
 <!doctype html>
 <html lang="ko">
 <head>
@@ -36,20 +42,18 @@ const app = http.createServer(function (req, res){
     <li><a href="/?id=CSS">CSS</a></li>
     <li><a href="/?id=JavaScript">JavaScript</a></li>
   </ol>
-  <h2>HTML</h2>
-  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-  <img src="coding.jpg" width="100%">
-  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-  </p>
+  <h2>${title}</h2>
+  <p>${data}</p>
 </body>
 </html>
-    `;
-    res.end(template);
+  `;
+        res.end(template);
+    });
+    //res.end(queryData.id);
     //res.end(fs.readFileSync(__dirname + url));
     //console.log(__dirname + req.url);    // 서버에서 이 파일의 내용을 그대로 보내겠다
     // res.end("hello egoing");
     // body로 보내는 부분
     //res.end("hello egoing");
-})
-
+});
 app.listen(3000);
