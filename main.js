fe369194b3d647bf342a56252f6c6e4d5d3d7831
jiyujung <s2019w39@e-mirim.hs.kr>
2021-03-25 09:45:28 +0900

    web2-24 글목록

--- main.js
+++ main.js
@@ -1,69 +1,74 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
 
 const app = http.createServer(function (request, response) {
-    const _url = request.url;
-    const queryData = url.parse(_url, true).query;
-    const pathname = url.parse(_url, true).pathname;
-    if (pathname === '/') {
-        if (queryData.id === undefined) {
-            //fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-            const title = 'Welcome';
-            const description = 'Hello, Node.js';
-            const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-            response.writeHead(200);
-            response.end(template);
-            //  });
-        } else {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-                const title = queryData.id;
-                const template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-                response.writeHead(200);
-                response.end(template);
-            });
+  const _url = request.url
+  const queryData = url.parse(_url, true).query
+  const pathname = url.parse(_url, true).pathname
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      const title = 'Welcome'
+      const description = 'Hello, Node.js'
+
+      fs.readdir('data/', function (err, data) {
+        let list = '<ul>'
+        for (let i = 0; i < data.length; i++) {
+          list += `<li><a href="/?id=${data[i]}"> ${data[i]} </a></li>`
         }
+        list += '</ul>'
+        const template = `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `
+        response.writeHead(200)
+        response.end(template)
+      })
+
     } else {
-        response.writeHead(404);
-        response.end('Not found');
+      fs.readdir('data/', function (err, data) {
+        let list = '<ul>'
+        for (let i = 0; i < data.length; i++) {
+          list += `<li><a href="/?id=${data[i]}"> ${data[i]} </a></li>`
+        }
+        list += '</ul>'
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          const title = queryData.id
+          const template = `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `
+          response.writeHead(200)
+          response.end(template)
+        })
+      })
+
     }
-
-
-});
-app.listen(3000);
+  } else {
+    response.writeHead(404)
+    response.end('Not found')
+  }
+})
+app.listen(3000)
